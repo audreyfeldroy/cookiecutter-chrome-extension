3fddb6f5a363c8817ae7f533ccb0178805f8adb7
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2015-10-07 23:10:06 -0700

    First commit, based on Page Redder.

---
+++ README.md
@@ -0,0 +1,3 @@
+# cookiecutter-chrome-extension
+
+A [Cookiecutter](https://github.com/audreyr/cookiecutter) template for a Google Chrome extension.
---
+++ {{cookiecutter.project_slug}}/background.js
@@ -0,0 +1,8 @@
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });
+});
